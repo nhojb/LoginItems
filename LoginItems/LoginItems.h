60d75cc977e3d5b36c37cc8c09087d3eef1ac157
John <john@olivetoast.com>
2016-11-22 11:44:11 +0000

    Initial Commit

---
+++ LoginItems/LoginItems.h
@@ -0,0 +1,19 @@
+//
+//  LoginItems.h
+//  LoginItems
+//
+//  Created by John on 22/11/2016.
+//  Copyright © 2016 Olive Toast Software Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LoginItems.
+FOUNDATION_EXPORT double LoginItemsVersionNumber;
+
+//! Project version string for LoginItems.
+FOUNDATION_EXPORT const unsigned char LoginItemsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginItems/PublicHeader.h>
+
+
